8760c4a6c7aedf1f68ddcd38db0f07796bfc3d53
gtpoint <elaouzariofficiel12@gmail.com>
2023-10-05 23:57:45 +0100

    created new project

---
+++ setup_mysql_test.sql
@@ -0,0 +1,10 @@
+-- Create the hbnb_test_db database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create the hbnb_test user if it does not exist, set the password, and grant privileges
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
